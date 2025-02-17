93ead866512c903efb2baee634f8832284e57712
Taksaorn20 <taksaorn.u@ku.th>
2025-02-17 16:39:15 +0700

    github update

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
 const app = express();
 
 app.use(bodyParser.json());
@@ -7,22 +8,42 @@ const port = 8000;
 
 // เก็บ user
 let users = []
-let counter = 1
+let conn = null
 
-// path = get /users
-app.get('/users', (req, res) => {
-    res.json(users);
+const initMySQL = async () => {
+    conn = await mysql.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : 'root',
+        database : 'webdb',
+        port : 8830
+    });
+}
+
+app.get('/testdb-new', async (req, res) => {
+    try {
+         const results = await conn.query('SELECT * FROM users')
+         res.json(results[0])
+        } catch (error) {
+        console.log('Error fetching users: ', error.message);
+        res.status(500).json({error: 'Error fetching users'});
+    }
 })
 
-// path = POST / user
-app.post('/user', (req, res) => {
+// path = get /users สำหรับ get users ทั้งหมด
+app.get('/users', async (req, res) => {
+    const results = await conn.query('SELECT * FROM users')
+    res.json(results[0]);
+})
+
+// path = POST / users สำหรับ create user ใหม่ บันทึกเข้าไป
+app.post('/users', async (req, res) => {
     let user = req.body;
-    user.id = counter
-    counter += 1
-    users.push(user);
+    const results = await conn.query('INSERT INTO users SET ?', users)
+    console.log('results', results)
     res.json({
-        message: "User created",
-        user: user
+            message: "User created",
+            data: results[0]
     });
 })
 
@@ -64,6 +85,7 @@ res.json({
     });
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(port, async (req, res) => {
+    await initMySQL();
+    console.log(`Server is running on port` + port);
 });
